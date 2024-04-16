1093a0318529e11207e4198f303e2b84255439f1
azouever <azouever@gmail.com>
2024-04-16 16:02:07 +0800

    feat:pip install use custom source

--- boot.sh
+++ boot.sh
@@ -1,5 +1,5 @@
 yum install -y python3
 python3 -m venv venv
 source venv/bin/activate
-pip3 install -r requirements.txt
+pip3 install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
 nohup python3 app.py >app.log 2>&1 &
